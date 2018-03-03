7ca11b573f894d5d107847e8d333e6972105c08e
miguelsilva96 <a74601@alunos.uminho.pt>
2018-03-03 14:15:20 +0000

    fix indentation issues

--- pos_hash.py
+++ pos_hash.py
@@ -8,28 +8,28 @@ free_cars = []
 
 
 class Ride:
-	def __init__ (self, init_x, init_y, fin_x, fin_y, e_start, l_finish):
-		global id_ride
-		self.id = id_ride
-		id_ride += 1
-		self.init_x = init_x
-		self.init_y = init_y
-		self.fin_x = fin_x
-		self.fin_y = fin_y
-		self.e_start = e_start
-		self.l_finish = l_finish
-		self.length = abs(fin_x - init_x) + abs(fin_y - fin_x)
-		# This score tells how many steps are still possible
-		# in order to start the ride and get points for it
-		self.score = self.l_finish - self.length
+    def __init__ (self, init_x, init_y, fin_x, fin_y, e_start, l_finish):
+        global id_ride
+        self.id = id_ride
+        id_ride += 1
+        self.init_x = init_x
+        self.init_y = init_y
+        self.fin_x = fin_x
+        self.fin_y = fin_y
+        self.e_start = e_start
+        self.l_finish = l_finish
+        self.length = abs(fin_x - init_x) + abs(fin_y - fin_x)
+        # This score tells how many steps are still possible
+        # in order to start the ride and get points for it
+        self.score = self.l_finish - self.length
 
-	# distance from input intersection to this ride's start point
-	def dist(self, pointx, pointy):
-		x = pointx-self.init_x
-		y = pointy-self.init_y
-		if x < 0: x = -x
-		if y < 0: y = -y
-		return x+y
+    # distance from input intersection to this ride's start point
+    def dist(self, pointx, pointy):
+        x = pointx-self.init_x
+        y = pointy-self.init_y
+        if x < 0: x = -x
+        if y < 0: y = -y
+        return x+y
 
 class Car:
     def __init__ (self):
@@ -93,47 +93,47 @@ def compute():
 # Write results to filename.out #
 #################################
 def write_file(filename):
-	global cars, rides
-	file = open(filename+'out', 'w')
-	string = ""
-	for i in range(len(cars)):
-		string += str(len(cars[i].rides))
-		for ride in cars[i].rides:
-			string += ' ' + str(ride.id)
-		string += '\n'
-	print(string)
-	file.write(string)
-	file.close()
+    global cars, rides
+    file = open(filename+'out', 'w')
+    string = ""
+    for i in range(len(cars)):
+        string += str(len(cars[i].rides))
+        for ride in cars[i].rides:
+            string += ' ' + str(ride.id)
+        string += '\n'
+    print(string)
+    file.write(string)
+    file.close()
 
 #################################
 #       Parse input file        #
 #################################
 def parse(filename):
-	global R, C, F, N, B, T, rides
-	file = open(filename, "r")
-	lines = file.readlines()
+    global R, C, F, N, B, T, rides
+    file = open(filename, "r")
+    lines = file.readlines()
 
-	firstLine = lines[0].rstrip().split(" ")
-	R = int(firstLine[0]); C = int(firstLine[1])
-	F = int(firstLine[2]); N = int(firstLine[3])
-	B = int(firstLine[4]); T = int(firstLine[5])
-	lines = lines[1:]
+    firstLine = lines[0].rstrip().split(" ")
+    R = int(firstLine[0]); C = int(firstLine[1])
+    F = int(firstLine[2]); N = int(firstLine[3])
+    B = int(firstLine[4]); T = int(firstLine[5])
+    lines = lines[1:]
 
-	for i in range(N):
-		ride = lines[i].rstrip().split(" ")
-		ride_info = Ride(int(ride[0]), int(ride[1]), int(ride[2]),
-						 int(ride[3]), int(ride[4]), int(ride[5]))
-		rides.append(ride_info)
+    for i in range(N):
+        ride = lines[i].rstrip().split(" ")
+        ride_info = Ride(int(ride[0]), int(ride[1]), int(ride[2]),
+                         int(ride[3]), int(ride[4]), int(ride[5]))
+        rides.append(ride_info)
 
-	for car in range(F):
-		cars.append(Car());
+    for car in range(F):
+        cars.append(Car());
 
 def main(argv):
-	filename = argv[1]
-	parse(filename)
-	compute()
-	write_file(filename[:len(filename)-2])
+    filename = argv[1]
+    parse(filename)
+    compute()
+    write_file(filename[:len(filename)-2])
 
 
 if __name__ == '__main__':
-	main(sys.argv)
+    main(sys.argv)
